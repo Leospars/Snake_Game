771e1d47213630fec414a7622c0bd2987cf9ab4d
leospars <leon.gayle012@gmail.com>
2024-02-03 23:00:15 -0500

    Moved to script and improved code layout to be more concise: moving UI features, that is buttons to snake_utils.js and Hamil Cycle to snakeAI_Hamil_Cycle, and made object (snake) functions more dynamic

--- script/snake.js
+++ script/snake.js
@@ -1,17 +1,20 @@
-export class Snake{
+// import { field_params } from "./snake_solid.js";
+// const {gridSize, cols, rows} = field_params;
+
+class Snake {
     xPos; 
     yPos;
     xVel = 0;
     yVel = 0;
     head = [0,0];
     body = [];
-    
-    initialPosition(){
+
+    //constructor for getting gridsize, rows and columns of field
+    initializePosition() {
         this.xPos = rand(3,cols/4) * gridSize;
         this.yPos = rand(0,rows/4) * gridSize;
-        this.xPos = this.yPos = gridSize*3;
-        this.head = [this.xPos,this.yPos]; 
-        // this.body = [[this.xPos-gridSize, this.yPos], [this.xPos-gridSize*2, this.yPos], [this.xPos-gridSize*3, this.yPos]];
+        this.head = [this.xPos, this.yPos];
+        this.body = [[this.xPos - gridSize, this.yPos], [this.xPos - gridSize * 2, this.yPos], [this.xPos - gridSize * 3, this.yPos]];
     }
 
     velocity (horizontal = 0, vertical = 0){
@@ -19,23 +22,28 @@ export class Snake{
         this.yVel = vertical;
     }
 
-    possibleMovements(snakePosition = snake.head){
-        this.right = [snake.head[0]+gridSize, snake.head[1]];
-        this.left = [snake.head[0]-gridSize, snake.head[1]];
-        this.up = [snake.head[0], snake.head[1]-gridSize];
-        this.down = [snake.head[0], snake.head[1]+gridSize];
+    //Generate snake possible movements from current position, initializing this.right, this.left, this.up, this.down
+    possibleMovements(snakePosition = this.head) {
+        this.right = [snakePosition[0] + gridSize, snakePosition[1]];
+        this.left = [snakePosition[0] - gridSize, snakePosition[1]];
+        this.up = [snakePosition[0], snakePosition[1] - gridSize];
+        this.down = [snakePosition[0], snakePosition[1] + gridSize];
     }
-    
-    updateSnake(){
-        let snakeIsMoving = !(snake.xVel == 0 && snake.yVel == 0);
-        if(snakeIsMoving){
+
+    update() {
+        let snakeIsMoving = !(this.xVel == 0 && this.yVel == 0);
+        if (snakeIsMoving) {
             //Update Snake Body
-            snake.body.unshift([snake.xPos, snake.yPos]);
-            snake.body.pop();
+            this.body.unshift([this.xPos, this.yPos]);
+            this.body.pop();
 
             //Update Snake Head
-            [snake.xPos, snake.yPos] = [snake.xPos + snake.xVel * gridSize, snake.yPos + snake.yVel * gridSize];
-            snake.head = [snake.xPos, snake.yPos];
+            [this.xPos, this.yPos] = [this.xPos + this.xVel * gridSize, this.yPos + this.yVel * gridSize];
+            this.head = [this.xPos, this.yPos];
         }
     }
+}
+
+function rand(smallest, largest) {
+    return Math.floor(Math.random() * (largest + 1 - smallest) + smallest);
 }
